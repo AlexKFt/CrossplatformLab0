b474797080fe207197c1c68e4147dc2d88dcba45
aleksandr.kushch <aleksandr.kushch@griser.ru>
2024-09-13 11:55:21 +0300

    Full lab 1

---
+++ Lab0/Data/Lab0Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lab0.Models;
+
+namespace Lab0.Data
+{
+    public class Lab0Context : DbContext
+    {
+        public Lab0Context (DbContextOptions<Lab0Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab0.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- Lab0/Program.cs
+++ Lab0/Program.cs
@@ -1,9 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Lab0.Data;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Lab0.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Lab0Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Lab0Context") ?? throw new InvalidOperationException("Connection string 'Lab0Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+
+	SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
